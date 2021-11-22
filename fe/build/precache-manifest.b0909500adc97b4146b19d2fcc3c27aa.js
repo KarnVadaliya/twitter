31bf2328fc5ca85748e171775e0cb5b3e2a9f244
Karn Vadaliya <karn.vadaliya@gmail.com>
2021-11-21 20:01:29 -0500

    All files

---
+++ fe/build/precache-manifest.b0909500adc97b4146b19d2fcc3c27aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37cdff0537f56af98dd66fae28d53e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5c2795a2364e4eb1f79",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "ebfac57d4a7aeb84bbd8",
+    "url": "/static/js/2.d2be65ae.chunk.js"
+  },
+  {
+    "revision": "2f90c8634b386e36b9c7e8327c2fcf69",
+    "url": "/static/js/2.d2be65ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c2795a2364e4eb1f79",
+    "url": "/static/js/main.e20df1d5.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
